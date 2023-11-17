b31b1ede51ff5c95d51b290ade5830de70975c55
cihan <b.cihancengiz@gmail.com>
2023-11-17 15:16:36 +0300

    complete

--- virtual-meet-template/src/hooks/useTokenMetadatas.ts
+++ virtual-meet-template/src/hooks/useTokenMetadatas.ts
@@ -1,15 +1,50 @@
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import Moralis from "moralis";
-import { TokenData } from "@/types/TokenData";
-import { apiKey, token_address_list } from "@/util/addresses";
-import { current_chain } from "@/util/chain";
+import {TokenData} from "@/types/TokenData";
+import {apiKey, token_address_list} from "@/util/addresses";
+import {current_chain} from "@/util/chain";
 
 export function useTokenMetadata() {
     const [message, setMessage] = useState("");
     const [loading, setLoading] = useState(true);
     const [tokens, setTokens] = useState<TokenData[]>([]);
 
-    const fetchTokenMetadata = useCallback(async () => {}, []);
+    const fetchTokenMetadata = useCallback(async () => {
+        try {
+            if (!Moralis.Core.isStarted) {
+                await Moralis.start({ apiKey });
+            }
+
+            // const token_metadatas = await Moralis.EvmApi.token.getTokenMetadata(
+            //     {
+            //         addresses: token_address_list,
+            //         chain: "chiliz",
+            //     }
+            // );
+            // setTokens(token_metadatas.toJSON());
+
+            let fullUrl = `https://deep-index.moralis.io/api/v2.2/erc20/metadata?chain=${current_chain}`;
+            token_address_list.forEach((address, index) => {
+                const addressParam = `&addresses%5B${index}%5D=${address}`;
+                fullUrl += addressParam;
+            });
+
+            const response = await fetch(fullUrl, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "X-API-Key": apiKey,
+                },
+            });
+            const data = await response.json();
+            setTokens(data);
+        } catch (e) {
+            setMessage("Error fetching token metadata");
+            console.log("Error fetching token metadata", e);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
     useEffect(() => {
         fetchTokenMetadata();
